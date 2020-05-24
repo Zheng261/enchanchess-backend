1e8ffcb2c759c7d7e0359853a060507c2a090997
natagoh <natagoh@stanford.edu>
2020-05-24 04:30:56 -0400

    sad nope

--- index.js
+++ index.js
@@ -45,18 +45,8 @@ whiteCards = cardJSON["whiteCards"]
 //   console.log(req.params)
 // })
 
-// const io = require('socket.io')(server);
-const io = require("socket.io")(server, {
-  handlePreflightRequest: (req, res) => {
-    const headers = {
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-      "Access-Control-Allow-Credentials": true
-    };
-    res.writeHead(200, headers);
-    res.end();
-  }
-});
+const io = require('socket.io')(server);
+
 
 io.on('connection', (socket) => { 
 	console.log("client connected", socket.id)
